d91b52f1b431a4ecb025cf1e101c351c8f9a0c12
elonder629 <ericlonder730@gmail.com>
2018-01-18 08:50:08 -0500

    Added a counter to count steps

--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -1,18 +1,23 @@
 package com.company;
 
 /**
- * Created by el693 on 1/18/18.
+ * Created by el693 (who is super cool and awesome) on 1/18/18.
  */
-public class TowersOfHanoi {
-    public TowersOfHanoi(int disks){
+class TowersOfHanoi {
+    public int step = 0;
+    TowersOfHanoi(int disks){
         moveTowers(disks-1,1,3,2);
     }
-    public static void moveTowers(int disk, int source, int dest, int spare){
-        if (disk == 0)
-            System.out.println("Move disk 1 to tower " + dest);
-        else{
+    private void moveTowers(int disk, int source, int dest, int spare){
+
+        if (disk == 0) {
+            step++;
+            System.out.println(step + ": Move disk 1 to tower " + dest);
+        }
+        else {
             moveTowers(disk-1, source, spare, dest);
-            System.out.println("Move disk " + (disk + 1) + " to tower " + dest);
+            step++;
+            System.out.println(step +": Move disk " + (disk + 1) + " to tower " + dest);
             moveTowers(disk-1, spare, dest, source);
         }
 
